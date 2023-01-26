3576c67551b924977b37ef608baa27dc47c50d22
victormanuelsanchez <123630481+victormanuelsanchez@users.noreply.github.com>
2023-01-26 12:26:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,8 +44,8 @@ resource "ibm_is_subnet" "subnet2" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance1" {
-  name    = "instance1"
+resource "ibm_is_instance" "instance10" {
+  name    = "instance10"
   image   = var.image
   profile = var.profile
   primary_network_interface {
@@ -58,8 +58,8 @@ resource "ibm_is_instance" "instance1" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance2" {
-  name    = "instance2"
+resource "ibm_is_instance" "instance20" {
+  name    = "instance20"
   image   = var.image
   profile = var.profile
   primary_network_interface {
